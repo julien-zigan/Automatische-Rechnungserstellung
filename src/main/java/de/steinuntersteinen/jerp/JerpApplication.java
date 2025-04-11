3793f09714f971addb854f2ba8a4c1b59b3725f5
Julien Zigan <zigan.julien@gmail.com>
2025-04-11 21:29:01 +0200

    saftey-commit: saving wip

--- src/main/java/de/steinuntersteinen/jerp/JerpApplication.java
+++ src/main/java/de/steinuntersteinen/jerp/JerpApplication.java
@@ -3,8 +3,10 @@ package de.steinuntersteinen.jerp;
 import de.steinuntersteinen.jerp.core.AppFacade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+ @EnableConfigurationProperties(StorageProperties.class)
 public class JerpApplication {
 	public static AppFacade app;
 
