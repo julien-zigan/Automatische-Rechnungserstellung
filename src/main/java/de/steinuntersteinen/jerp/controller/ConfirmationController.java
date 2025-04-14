cb2fd1d0c5956a1f2341051c1464e76323cb91d9
Julien Zigan <zigan.julien@gmail.com>
2025-04-14 10:19:06 +0200

    implement invoice building on confirmation upload

--- src/main/java/de/steinuntersteinen/jerp/controller/ConfirmationController.java
+++ src/main/java/de/steinuntersteinen/jerp/controller/ConfirmationController.java
@@ -1,7 +1,12 @@
 package de.steinuntersteinen.jerp.controller;
 
+import de.steinuntersteinen.jerp.core.Confirmation.Confirmation;
 import de.steinuntersteinen.jerp.core.Invoice.Invoice;
+import de.steinuntersteinen.jerp.core.Invoice.InvoiceBuilder;
+import de.steinuntersteinen.jerp.core.Persistence.DataBase;
+import de.steinuntersteinen.jerp.core.Persistence.User;
 import de.steinuntersteinen.jerp.storage.StorageService;
+import org.apache.tomcat.jni.FileInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
 
 @Controller
 public class ConfirmationController {
@@ -23,8 +30,20 @@ public class ConfirmationController {
     @PostMapping("/uploadConfirmation")
     public String handleFileUpload(@RequestParam("file")MultipartFile file, Model model, RedirectAttributes redirectAttributes) {
         storageService.store(file);
-        model.addAttribute("invoice",new Invoice());
-        redirectAttributes.addFlashAttribute("conf_loaded", "true");
+        try {
+            User user = DataBase.loadUser();
+            File confirmationFile = new File(user.getPathToDocumentDirectory() +
+                file.getOriginalFilename());
+            Confirmation confirmation = Confirmation.from(confirmationFile);
+            Invoice invoice = InvoiceBuilder.build(user, confirmation);
+            model.addAttribute("invoice", invoice);
+
+            redirectAttributes.addFlashAttribute("confirmationLoaded", "true");
+            redirectAttributes.addFlashAttribute("invoiceCreated", "true");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         return "/index";
     }
 
