d28b335ea9cfaee729a15d1fc3b06621d14a62b6
Julien Zigan <zigan.julien@gmail.com>
2025-04-19 20:33:42 +0200

    -

---
+++ src/main/resources/styletemplates/script.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 9; i++) {
+    getElementById(i).registerListeners
+}
+                function registerListeners(id) {
+                  document
+                    .getElementById(id)
+                    .addEventListener("change", updateSubmit);
+                }
+
+                function updateSumTotal() {
+for (let i = 0; i < 9; i++) {
+    getElementById(i).registerListeners
+}
+
+                  display.innerHTML = duration * rate + travelFee + " €";
+                }
